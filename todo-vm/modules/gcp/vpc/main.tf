438d96b1db924f9151ec3ae8c8c3e9dd05f2a243
HoangDuonng <Nguuyen1801@gmail.com>
2026-07-17 16:26:32 +0700

    feat(vpc): initialize terraform module for vpc and subnets

---
+++ todo-vm/modules/gcp/vpc/main.tf
@@ -0,0 +1,24 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+  project                 = var.project_id
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name                     = var.subnet_name
+  ip_cidr_range            = var.subnet_cidr
+  region                   = var.region
+  network                  = google_compute_network.vpc.id
+  project                  = var.project_id
+  private_ip_google_access = true
+
+  secondary_ip_range {
+    range_name    = var.pods_cidr_name
+    ip_cidr_range = var.pods_cidr
+  }
+
+  secondary_ip_range {
+    range_name    = var.services_cidr_name
+    ip_cidr_range = var.services_cidr
+  }
+}
